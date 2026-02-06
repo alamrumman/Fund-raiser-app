09f7fdeb0d10fe558e7144e2816d3523a1a6ac61
Rumi-stack <alamrumman039@gmail.com>
2026-02-06 23:14:30 +0530

    Improved pop up UI and make quick redirects as required

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "@fontsource/inter"; // 400
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
@@ -10,6 +12,8 @@ import "@fontsource/inter/800.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+      <BrowserRouter>
     <App />
+  </BrowserRouter>
   </StrictMode>,
 );
