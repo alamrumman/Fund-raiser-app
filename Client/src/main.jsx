a0336bfab3abf08c0a765b0f0029dbe7fb351153
Rumi-stack <alamrumman039@gmail.com>
2026-02-07 16:47:13 +0530

    Made separate component for forms , improved Select Ui and provided modern view

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -9,7 +9,18 @@ import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/800.css";
+const COLORS = {
+  bg: "#f9e8c9",
+  white: "#FFFFFF",
 
+  textPrimary: "#1F1F1F",
+  textSecondary: "#6B6B6B",
+
+  primary: "#2FBF71",
+  primarySoft: "#E6F7ED",
+
+  border: "#E6E6E6",
+};
 createRoot(document.getElementById("root")).render(
   <StrictMode>
       <BrowserRouter>
